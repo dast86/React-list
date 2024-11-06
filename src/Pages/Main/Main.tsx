c9e99a26c75523f3ac45b322987c18d7a9d237fc
Alex Popov <popov_a_a_86@mail.ru>
2024-11-06 20:24:07 +0500

    добавил axios

--- src/Pages/Main/Main.tsx
+++ src/Pages/Main/Main.tsx
@@ -1,29 +1,59 @@
-import dony from "../../img/1.jpg"
-import noActive from "../../img/svg/14.svg"
+import dony from "../../img/1.jpg";
+import noActive from "../../img/svg/14.svg";
 // import active from "../../img/svg/5.svg"
-import styles from "./styles.module.css"
+import axios from "axios";
+import styles from "./styles.module.css";
+import { useEffect, useState } from "react";
 
-const Main = () =>{
-    return(
-        <main className={styles.conteiner}>
-                <ul className={styles.title}>
-                    <li>Фото</li>
-                    <li>Псевдоним</li>
-                    <li>Настоящее имя</li>
-                    <li>Контакты для связи</li>
-                    <li></li>
-                </ul>
+const Main = () => {
+  const [users, setUsers] = useState([]);
+  const [loding, setLoding] = useState(true);
 
-                <ul className={styles.list}>
+  const url = "https://jsonplaceholder.typicode.com/users";
+  const getUser = async () => {
+    try {
+      const response = await axios.get(url);
+      setUsers(response.data);
+    } catch (error) {
+      console.log(`Ошибка:`, error);
+    } finally {
+      setLoding(false);
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+  return (
+    <main className={styles.conteiner}>
+      <ul className={styles.title}>
+        <li>Фото</li>
+        <li>Псевдоним</li>
+        <li>Настоящее имя</li>
+        <li>Контакты для связи</li>
+        <li></li>
+      </ul>
+
+      { users.map((user) => (
+        <ul className={styles.list} key={user.id}>
+          <li>
+            <img className={styles.image} src={dony} alt="" />
+          </li>
+          <li> {user.username}</li>
+          <li> {user.name} </li>
+          <li>{user.email}</li>
+          <li> <img src={noActive}  className={styles.svg} alt="" /></li>
+        </ul>
+      ))}
+      {/* <ul className={styles.list}>
                     <li><img className={styles.image} src={dony} alt="" /></li>
                     <li> Дони-Чёп</li>
                     <li> Донателло </li>
                     <li>doni@mail.com</li>
                     <li> <img src={noActive}  className={styles.svg} alt="" /></li>
-                </ul>
-        </main>
-    )
-}
+                </ul> */}
+    </main>
+  );
+};
 
-
-export default Main
+export default Main;
